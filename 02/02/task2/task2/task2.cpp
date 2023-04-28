e13d84937ea01cb7d279967cd7b09339cb2a14a6
Kosmodesant <smirnov.nal@outlook.com>
2023-04-28 20:30:23 +0300

    release 1.0

---
+++ 02/02/task2/task2/task2.cpp
@@ -0,0 +1,45 @@
+﻿
+
+//Синтаксическая ошибка 
+#include <iostream>
+
+int main()
+{
+    int a = 100;
+    int b = 200;
+    int c = 2 * (a + b;// пропущена скобка
+
+    /*
+    int a = 100;
+    int b = 200;
+    int c = 2 * (a + b);//  верно;*/
+
+
+//Семантическая ошибка
+#include <iostream>
+
+    int main()
+    {
+        int num = 0;
+        while (num < 100) //бесконечныцй цикл
+        {
+            std::cout << count << ' ';
+        }
+        return 0;
+    }
+
+    //#include <iostream>
+    //
+    //    int main()
+    //    {
+    //        int num = 0;
+    //     ...
+    // 
+    //        while (num == 100) // четкое условие
+    //        {
+    //            std::cout << count << ' ';
+    //        }
+    //        return 0;
+    //    }
+
+
