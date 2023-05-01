fa59d2c03b0fe49b63a3e6812383a36756dea1b2
Kosmodesant <smirnov.nal@outlook.com>
2023-05-01 11:32:14 +0300

    release 1.0

--- 01/task1/task1/task1.cpp
+++ 01/task1/task1/task1.cpp
@@ -7,7 +7,9 @@ using namespace std;
 void print(const vector<int>& v, const string& str)
 {
 	cout << str << " данные: ";
-	for (const auto& val : v) cout << val << " ";
+	for (const auto& val : v) {
+		cout << val << " ";
+	}
 	cout << endl;
 }
 
@@ -16,7 +18,7 @@ int main(int argc, char** argv)
 	setlocale(LC_ALL, "Russian");
 	vector<int> vect{ 4, 7, 9, 14, 12 };
 	print(vect, "Входные");
-	for_each(begin(vect), end(vect),[](int& value) { if (value % 2) value *= 3; });
+	for_each(begin(vect), end(vect), [](int& value) { if (value % 2) value *= 3; });
 	print(vect, "Выходные");
 	return 0;
 }
