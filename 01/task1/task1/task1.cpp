bcc9b1e53c5850c5a2a3c75db392272962896e5b
Kosmodesant <smirnov.nal@outlook.com>
2023-05-01 11:34:33 +0300

    release 2.0

--- 01/task1/task1/task1.cpp
+++ 01/task1/task1/task1.cpp
@@ -18,7 +18,8 @@ int main(int argc, char** argv)
 	setlocale(LC_ALL, "Russian");
 	vector<int> vect{ 4, 7, 9, 14, 12 };
 	print(vect, "Входные");
-	for_each(begin(vect), end(vect), [](int& value) { if (value % 2) value *= 3; });
+	for_each(begin(vect), end(vect), [](int& value) { 
+		if (value % 2) value *= 3; });
 	print(vect, "Выходные");
 	return 0;
 }
