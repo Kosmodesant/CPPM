593fd92d4591950f6f689eb2dff759762d6123e7
Kosmodesant <smirnov.nal@outlook.com>
2023-05-02 15:46:36 +0300

    release 1.0

---
+++ 01/02/task2ver2/task2ver2/task2ver2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <variant>
+#include <vector>
+
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+    std::srand(std::time(nullptr));
+    int random_variable = std::rand() % 3;
+
+    std::variant<int, std::string, std::vector<int>> result;
+    switch (random_variable)
+    {
+    case 0:
+        result = 5;
+        break;
+    case 1:
+        result = "string";
+        break;
+    case 2:
+        result = std::vector<int>{ 1, 2, 3, 4, 5 };
+        break;
+    default:
+        break;
+    }
+    return result;
+}
+
+int main() {
+    std::variant<int, std::string, std::vector<int>> result = get_variant();
+
+    if (std::holds_alternative<int>(result)) {
+        int value = std::get<int>(result);
+        std::cout << value * 2 << std::endl;
+    }
+    else if (std::holds_alternative<std::string>(result)) {
+        std::string value = std::get<std::string>(result);
+        std::cout << value << std::endl;
+    }
+    else if (std::holds_alternative<std::vector<int>>(result)) {
+        std::vector<int> value = std::get<std::vector<int>>(result);
+        for (int i : value) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
