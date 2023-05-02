cd66923d93dbfcb8ee90462bf390c418bf705cfe
Kosmodesant <smirnov.nal@outlook.com>
2023-05-02 15:57:48 +0300

    release 1.0

---
+++ 03/01/task1/task1/SmartArray.cpp
@@ -0,0 +1,39 @@
+#include <stdexcept>
+#include "SmartArray.h"
+
+SmartArray::SmartArray(size_t size)
+{
+	m_elements = new int[size];
+	m_size = size;
+}
+
+SmartArray::~SmartArray()
+{
+	delete[] m_elements;
+}
+
+void SmartArray::addElement(int value)
+{
+	if (m_head >= m_size)
+	{
+		throw std::out_of_range("Попытка добавления элемента с превышением размера массива");
+	}
+
+	m_elements[m_head++] = value;
+}
+
+
+int SmartArray::getElement(size_t index)
+{
+	if (index >= m_head)
+	{
+		throw std::out_of_range("Индекс элемента массива вне допустимого диапазона");
+	}
+
+	return m_elements[index];
+}
+
+size_t SmartArray::getLength()
+{
+	return m_head;
+}
