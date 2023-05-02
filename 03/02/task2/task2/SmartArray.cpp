83f00f270847e6d95b82cd392055b1c51959f905
Kosmodesant <smirnov.nal@outlook.com>
2023-05-02 16:19:59 +0300

    release 1.0

---
+++ 03/02/task2/task2/SmartArray.cpp
@@ -0,0 +1,72 @@
+#include <stdexcept>
+#include "SmartArray.h"
+
+SmartArray::SmartArray(size_t size)
+{
+	m_elements = new int[size];
+	m_size = size;
+}
+
+SmartArray::SmartArray(const SmartArray& src)
+{
+	copyFrom(src);
+}
+
+SmartArray& SmartArray::operator =(const SmartArray& src)
+{
+	if (&src != this)
+	{
+		cleanup();
+		copyFrom(src);
+	}
+
+	return *this;
+}
+
+SmartArray::~SmartArray()
+{
+	cleanup();
+}
+
+void SmartArray::addElement(int value)
+{
+	if (m_head >= m_size)
+	{
+		throw std::out_of_range("Попытка добавления элемента с превышением размера массива");
+	}
+
+	m_elements[m_head++] = value;
+}
+
+
+int SmartArray::getElement(size_t index)
+{
+	if (index >= m_head)
+	{
+		throw std::out_of_range("Индекс элемента массива вне допустимого диапазона");
+	}
+
+	return m_elements[index];
+}
+
+size_t SmartArray::getLength()
+{
+	return m_head;
+}
+
+void SmartArray::copyFrom(const SmartArray& src)
+{
+	m_elements = new int[src.m_size];
+	for (auto i = 0; i < src.m_head; ++i)
+	{
+		m_elements[i] = src.m_elements[i];
+	}
+
+	m_head = src.m_head;
+	m_size = src.m_size;
+}
+
+void SmartArray::cleanup()
+{
+	delete[] m_elements;
+}
