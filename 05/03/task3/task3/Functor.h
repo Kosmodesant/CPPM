7c823a80f99a227d3fcaae07dbb53db8202f18e6
Nikita <element_90@bk.ru>
2023-05-15 14:06:36 +0300

    release 1.0

---
+++ 05/03/task3/task3/Functor.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+
+class Functor
+{
+private:
+	int _sum;
+	int _count;
+public:
+	Functor();
+	void operator() (std::vector<int> v);
+	int get_sum();
+	int get_count();
+};
