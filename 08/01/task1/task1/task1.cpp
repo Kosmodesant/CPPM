53301a65280827f2dcfc386db3856c872b2e0a9b
Nikita <element_90@bk.ru>
2023-06-03 18:39:22 +0300

    release 1.0

---
+++ 08/01/task1/task1/task1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void delDubls(vector<int>& vec)
+{ 
+	sort(vec.begin(), vec.end());
+	vec.erase((unique(vec.begin(), vec.end())), vec.end());
+}
+
+template <typename VType>
+ostream& operator<< (ostream& out, const vector<VType>& vect)
+{
+	for (const auto& val : vect) out << val << " ";
+	return out;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	vector<int> vec{ 1, 1, 2, 5, 6, 1, 2, 4 };
+	cout << "[IN]: " << vec << endl;
+	delDubls(vec);
+	cout << "[OUT]: " << vec << endl;
+	return 0;
+}
