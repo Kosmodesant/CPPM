7aaee86b1af0278cceb07f34f36b4426b7543329
Nikita <element_90@bk.ru>
2023-06-03 18:54:56 +0300

    release 1.0

---
+++ 07/02/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	size_t count = 0;
+	cout << "[IN]: " << endl;
+	cin >> count;
+	set<int, greater<int>> valueSet;
+	for (size_t i = 0; i < count; ++i)
+	{
+		int v;
+		cin >> v;
+		valueSet.insert(v);
+	}
+
+	cout << "[OUT]:" << endl;
+	for (const auto& v : valueSet)
+	{
+		cout << v << endl;
+	}
+}
