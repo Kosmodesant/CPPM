a0962dca96dd5f685b9be117fd677b48ea87e6ce
Nikita <element_90@bk.ru>
2023-06-03 18:40:20 +0300

    release 1.0

---
+++ 08/02/task2/task2/task2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+template <class N>
+void print_container(const N& cont, const string& str)
+{
+	cout << str << ": ";
+
+	for (auto it = cont.cbegin(); it != cont.cend(); ++it)
+		cout << *it << " ";
+	cout << endl;
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	set<string> test_set = { "one", "two", "three", "four" };
+	list<string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list, "test_list");
+	vector<string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector, "test_vector");
+	return 0;
+}
