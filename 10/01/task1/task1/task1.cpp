413a5eebf197a89649b9ef582a681ed82acb7650
Nikita <element_90@bk.ru>
2023-06-04 12:45:41 +0300

    release 1.0

--- 10/01/task1/task1/task1.cpp
+++ 10/01/task1/task1/task1.cpp
@@ -1,41 +1,40 @@
 ﻿#include <iostream>
-#include <string>
-#include <unordered_map>
-#include <map>
+#include <vector>
+#include <memory>
 
 using namespace std;
 
+class tridiagonal_matrix
+{
+public:
+    vector<double> m_down;
+    vector<double> m_upper;
+    vector<double> m_middle;
+    tridiagonal_matrix(
+        const vector<double>& down,
+        const vector<double>& upper,
+        const vector<double>& middle) :
+        m_down{ down }, m_upper{ upper }, m_middle{ middle }
+    {};
+    ~tridiagonal_matrix() { cout << "destructor called\n"; }
+
+    unique_ptr<tridiagonal_matrix> clone() const {
+        return make_unique<tridiagonal_matrix>(m_down, m_upper, m_middle);
+    }
+};
+
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+    auto down = vector<double>{ -4.0, 5.0 };
+    auto upper = vector<double>{ 14.0, 8.0 };
+    auto middle = vector<double>{ 3.0, 1.0, 7.0 };
+    auto matrix = make_unique<tridiagonal_matrix>(
+        down,
+        upper,
+        middle
+        );
 
-	string input;
-	cout << "[IN]: ";
-	getline(cin, input);
+    auto matrix_clone = matrix->clone();
 
-	unordered_map<char, unsigned int> charMap;
-	for (const auto& c : input)
-	{
-		auto it = charMap.find(c);
-
-		if (it == charMap.end())
-		{
-			charMap.insert({ c, 1 });
-		}
-		else
-		{
-			++((*it).second);
-		}
-	}
-	multimap<unsigned int, char, greater<char>> charOrderMap;
-	for (const auto& [c, count] : charMap)
-	{
-		charOrderMap.insert({ count, c });
-	}
-
-	cout << "[OUT]:" << endl;
-	for (const auto& [count, c] : charOrderMap)
-	{
-		cout << c << ": " << count << endl;
-	}
+    return 0;
 }
